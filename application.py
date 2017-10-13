36b3b1ed4196de5c6bbd92c881bff981c92d8f7c
Pranjal Patel <pranjalpatelpp@gmail.com>
2017-10-13 18:43:22 -0400

    Changed the port to from 5000 to 8000

--- application.py
+++ application.py
@@ -298,4 +298,4 @@ def dated_url_for(endpoint, **values):
 if __name__ == '__main__':
     app.debug = True
     app.secret_key = 'super_secret_key'
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
