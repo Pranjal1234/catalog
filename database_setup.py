d8828bb8c0643ea694869d558dbf49b3612fdc6b
Pranjal Patel <pranjalpatelpp@gmail.com>
2017-10-13 17:14:29 -0400

    database_setup.py cleared

--- database_setup.py
+++ database_setup.py
@@ -5,16 +5,20 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from passlib.apps import custom_app_context as pwd_context
-import random, string
-from itsdangerous import(TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
+import random
+import string
+from itsdangerous import(TimedJSONWebSignatureSerializer as
+                         Serializer, BadSignature, SignatureExpired)
 
 Base = declarative_base()
 
 # This will be used as the secret key to create and verify your token
-secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+secret_key = ''.join(random.choice(string.ascii_uppercase +
+                     string.digits) for x in xrange(32))
+
 
 class User(Base):
-    __tablename__='user'
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False, unique=True)
     email = Column(String(250), nullable=False)
@@ -23,14 +27,16 @@ class User(Base):
     def hash_password(self, password):
 
         self.password_hash = pwd_context.encrypt(password)
+
     def verify_password(self, password):
         return pwd_context.verify(password, self.password_hash)
-    #Add a method to generate auth tokens here
+    # Add a method to generate auth tokens here
+
     def generate_auth_token(self, expiration=600):
-        s = Serializer(secret_key, expires_in = expiration)
-        return s.dumps({'id': self.id })
-        
-    #Add a method to verify auth tokens here
+        s = Serializer(secret_key, expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+    # Add a method to verify auth tokens here
     @staticmethod
     def verify_auth_token(token):
         s = Serializer(secret_key)
@@ -42,27 +48,27 @@ class User(Base):
             return None
         user_id = data['id']
         return user_id
-        
+
 
 class Catalog(Base):
-    __tablename__='catalog'
-    name = Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
+    __tablename__ = 'catalog'
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
     @property
     def serialize(self):
         return {
-            'id' : self.id,
-            'name' : self.name,
+            'id': self.id,
+            'name': self.name
         }
 
 
 class Item(Base):
-    __tablename__='item'
-    name = Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
+    __tablename__ = 'item'
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     description = Column(String(280))
     category_id = Column(Integer, ForeignKey('catalog.id'))
     catalog = relationship(Catalog)
@@ -72,10 +78,10 @@ class Item(Base):
     @property
     def serialize(self):
         return {
-            'cat_id' : self.category_id,
-            'description' : self.description,
-            'id' : self.id,
-            'title' : self.name
+            'cat_id': self.category_id,
+            'description': self.description,
+            'id': self.id,
+            'title': self.name
         }
 
 engine = create_engine('sqlite:///catalog.db')
